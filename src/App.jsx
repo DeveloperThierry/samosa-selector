f82e43496d4f0c8c30017fd760a2d19b2bdb4c9f
developerthierry <developerthierry@gmail.com>
2026-06-09 01:38:47 +0000

    Create the User Interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
-
+import samosa from './assets/samosa.png'
 const App = () => {
 
   return (
     <div className="App">
-
+      <h1>Samosa Selector</h1>
+      <h2>Count:</h2>
+      {/* <img className="samosa" src="/assets/samosa.png" alt="samosa" /> */}
+      <img className="samosa" src={samosa} alt="samosa" />
     </div>
   )
 }
