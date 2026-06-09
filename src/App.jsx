61de12e615c93c17b78f82aca7293472d9b46b2b
developerthierry <developerthierry@gmail.com>
2026-06-09 01:59:47 +0000

    Add Upgrades

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,23 @@ const App = () => {
       <h2>Count:{count}</h2>
       {/* <img className="samosa" src="/assets/samosa.png" alt="samosa" /> */}
       <img className="samosa" src={samosa} alt="samosa" onClick={updateCount} />
+      <div className="container">
+        <div className="upgrade">
+          <h3>Double Stuffed 👯‍♀️</h3>
+          <p>2x per click</p>
+          <button>10 samosas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Party Pack 🎉</h3>
+          <p>5x per click</p>
+          <button>100 samosas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Full Feast 👩🏽‍🍳</h3>
+          <p>10x per click</p>
+          <button>1000 samosas</button>
+        </div>
+      </div>
     </div>
   )
 }
