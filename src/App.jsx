5249140bdcaee8a230c0717d1e91e6d9a4215c73
developerthierry <developerthierry@gmail.com>
2026-06-09 02:09:21 +0000

    Handle Upgrades

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,24 @@ const App = () => {
   const updateCount = () => {
     setCount(count + multiplier)
   }
+
+  const buyDoubleStuffed = () => {
+    if(count >= 10){
+      setMultiplier(multiplier * 2)
+    }
+  }
+
+  const buyPartyPack = () => {
+    if(count >= 100){
+      setMultiplier(multiplier * 5)
+    }
+  }
+
+  const buyFullFeast = () => {
+    if(count>=1000){
+      setMultiplier(multiplier * 10)
+    }
+  }
   return (
     <div className="App">
       <h1>Samosa Selector</h1>
@@ -19,17 +37,17 @@ const App = () => {
         <div className="upgrade">
           <h3>Double Stuffed 👯‍♀️</h3>
           <p>2x per click</p>
-          <button>10 samosas</button>
+          <button onClick={buyDoubleStuffed}>10 samosas</button>
         </div>
         <div className="upgrade">
           <h3>Party Pack 🎉</h3>
           <p>5x per click</p>
-          <button>100 samosas</button>
+          <button onClick={buyPartyPack}>100 samosas</button>
         </div>
         <div className="upgrade">
           <h3>Full Feast 👩🏽‍🍳</h3>
           <p>10x per click</p>
-          <button>1000 samosas</button>
+          <button onClick={buyFullFeast}>1000 samosas</button>
         </div>
       </div>
     </div>
